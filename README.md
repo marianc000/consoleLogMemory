e7462e1e3da98d6f209a673797a7187f7c647d4b
Marian <marian.caikovski@gmail.com>
2022-02-02 18:50:12 +0100

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# How much memory is available for a web page?
+
+Sample code for my post https://marian-caikovski.medium.com/how-much-memory-is-available-for-a-web-page-7a52d4f6e3be about available memory limits in a browser. How to use up a browser’s memory so that it crashes?
---
+++ js/main.js
@@ -0,0 +1,20 @@
+const MB = 1_000_000;
+
+function row(i, bytes) {
+  return `<div class='data'><div>${i}</div><div>${Math.floor(bytes / MB)}</div></div>`
+}
+
+runBtn.onclick = () => {
+  runBtn.remove();
+  addString();
+}
+
+async function addString(i = 1) {
+  console.log({ a: 'X'.repeat(50 * MB) });
+  const memory = await performance.measureUserAgentSpecificMemory();
+  outputDiv.insertAdjacentHTML('beforeend',
+    `<div class='data'><div>${i}</div><div>${Math.floor(memory.bytes / MB)}</div></div>`);
+
+  //if (i < 40)
+  setTimeout(() => addString(++i), 1000);
+}
